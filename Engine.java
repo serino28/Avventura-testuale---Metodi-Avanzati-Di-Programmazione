fdc1d3ad89b984ae199caf872d8ee572db3fc191
AntonioSerino© <61455975+serino28@users.noreply.github.com>
2021-07-10 20:01:55 +0200

    Add files via upload

--- Engine.java
+++ Engine.java
@@ -1,1 +1,87 @@
+package serinoTeam.adventureserino;
 
+import java.util.Iterator;
+import java.util.Queue;
+import serinoTeam.adventureserino.parser.ParserOutput;
+import serinoTeam.adventureserino.parser.Parser;
+import serinoTeam.adventureserino.games.SerinoAdventure;
+import java.util.Scanner;
+import serinoTeam.adventureserino.type.CommandType;
+
+/**
+ *
+ * @author Antonio Serino
+ */
+public class Engine {
+
+    private final GameDescription game;
+
+    private final Parser parser;
+
+    public Engine(GameDescription game) {
+        this.game = game;
+        try {
+            this.game.init();
+        } catch (Exception ex) {
+            System.err.println(ex);
+        }
+        parser = new Parser();
+    }
+
+    public GameDescription getGame() {
+        return game;
+    }
+
+    public Parser getParser() {
+        return parser;
+    }
+
+    public void run() {
+        if (game.isTimerOn()){
+            game.getCronometro().avanza();
+        }
+        System.out.println(game.getCurrentRoom().getName());
+        System.out.println("> "+game.getCurrentRoom().getDescription());
+        System.out.println("-------------------------------------------------------------");
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            if (game.isTimerOn() && game.getCronometro().leggi()>game.getTimer()) {
+                System.out.println("Tempo esaurito!");
+                break;
+            } else if (game.isTimerOn()){
+                System.out.println("[Ti rimangono "+((game.getTimer()-game.getCronometro().leggi())/1000) +" secondi]");
+            }
+            String command = scanner.nextLine();
+            listParser : { Queue<ParserOutput> movements = parser.parse(command, game.getCommands(), game.getInventory(), game.getCurrentRoom());
+                for (Iterator<ParserOutput> iter = movements.iterator(); iter.hasNext();) {
+                    ParserOutput p = iter.next();
+                    try {
+                        if (p.getCommand() != null && p.getCommand().getType() == CommandType.END) {
+                            System.out.println("Hai abbandonato il gioco!");
+                            break;
+                        } else {
+                            game.nextMove(p, System.out);
+                            iter.remove();
+                            System.out.println("-------------------------------------------------------------");
+                        }
+                    }
+                    catch (Exception e) {
+                        System.out.println("Restart da: "+game.getCurrentRoom().getName());
+                        System.out.println("> "+game.getCurrentRoom().getDescription());
+                        System.out.println("-------------------------------------------------------------");
+                        break listParser;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Engine engine = new Engine(new SerinoAdventure());
+        engine.run();
+    }
+
+}
--- GameDescription.java
+++ GameDescription.java
@@ -1,1 +1,73 @@
+package serinoTeam.adventureserino;
 
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import serinoTeam.adventureserino.parser.ParserOutput;
+import serinoTeam.adventureserino.type.AdvObject;
+import serinoTeam.adventureserino.type.Command;
+import serinoTeam.adventureserino.type.Inventory;
+import serinoTeam.adventureserino.type.Room;
+import serinoTeam.adventureserino.type.Timer;
+
+public abstract class GameDescription {
+    private final List<Room> rooms = new ArrayList();
+    private final List<Command> commands = new ArrayList();
+    private final Inventory inventory = new Inventory();
+    private final Timer cronometro = new Timer();
+    private Room currentRoom;
+    private int timer;
+    private boolean timerOn = false;
+
+    public GameDescription() {
+    }
+
+    public boolean isTimerOn() {
+        return this.timerOn;
+    }
+
+    public void setTimerOn(boolean timerOn) {
+        this.timerOn = timerOn;
+    }
+
+    public Timer getCronometro() {
+        return this.cronometro;
+    }
+
+    public int getTimer() {
+        return this.timer;
+    }
+
+    public void setTimer(int timer) {
+        this.timerOn = true;
+        this.timer = timer;
+    }
+
+    public void startCronometro() {
+        this.cronometro.avanza();
+    }
+
+    public List<Room> getRooms() {
+        return this.rooms;
+    }
+
+    public List<Command> getCommands() {
+        return this.commands;
+    }
+
+    public Room getCurrentRoom() {
+        return this.currentRoom;
+    }
+
+    public void setCurrentRoom(Room currentRoom) {
+        this.currentRoom = currentRoom;
+    }
+
+    public List<AdvObject> getInventory() {
+        return this.inventory.getList();
+    }
+
+    public abstract void init() throws Exception;
+
+    public abstract void nextMove(ParserOutput var1, PrintStream var2);
+}
